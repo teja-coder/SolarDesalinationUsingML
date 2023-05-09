d952e0e9dc8d0df09a57ae329d073acc0827065c
Pendyala Divya Teja <82772829+teja-coder@users.noreply.github.com>
2023-05-09 15:06:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+model = pickle.load(open('model.sav', 'rb'))
+
+st.title('Prediction of Hourly Productivity')
+st.sidebar.header('Input Parameters')
+image = Image.open('img.jpg')
+st.image(image,'')
+
+def user_report():
+  T_water = st.sidebar.slider('Water Temperature',20.0,70.0,1.0)
+  T_inside_glass = st.sidebar.slider('Inside Glass Temperature',30.0,70.0,0.01)
+  Ambient_Temperature = st.sidebar.slider('Ambient Temperature',20.0,40.0,0.01)
+  Irradiance = st.sidebar.slider('Irradiance',0.0,1000.0,0.01)
+  Mirror = st.sidebar.slider('Mirror',0,3,0)
+
+  user_report_data = {
+    'T_water': T_water,
+    'T_inside_glass': T_inside_glass,
+    'Ambient_Temperature': Ambient_Temperature,
+    'Irradiance': Irradiance,
+    'Mirror': Mirror  
+  }
+
+  report_data = pd.DataFrame(user_report_data, index=[0])
+  return report_data
+
+user_data = user_report()
+st.header('Input Parameters')
+st.write(user_data)
+
+hourly_productivity = model.predict(user_data)
+st.subheader('Hourly Productivity')
+st.subheader(str(np.round(hourly_productivity[0], 4)) + ' Kg/m2')
